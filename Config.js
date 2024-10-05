b9f954ea1278461378bed497e17bb86b328741c7
gece901 <ffkiran615@gmail.com>
2024-10-05 14:26:59 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // Botu Kullanmadan README.md dosyasını okuyun!
-    "Token": ""
+    "Token": "MTI5MTg3NTMyOTc3MzY2NjMwNA.Gy-4ym.Sdy-T_XIIc0_6KH4lK5D6dz92TF9wVqWH31Fk4"
     // Botu Kullanmadan README.md dosyasını okuyun!
 }
